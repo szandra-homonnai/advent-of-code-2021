3c00cd710f23e6ddd78eeee624ad6166b3c0f9ec
Szandra <szandra.homonnai@gmail.com>
2022-01-04 16:23:43 +0100

    feat: add day 1 task 2 solution

--- src/days/day-1.ts
+++ src/days/day-1.ts
@@ -9,19 +9,37 @@ export function day1(): void {
     }
 
     const data: string[] = result.split('\r\n');
+    const sumArray: number[] = [];
     let largerCount: number = 0;
+    let sumLargerCount: number = 0;
 
     for (let index = 0; index < data.length; index++) {
       const previous = data[index - 1] ? parseInt(data[index - 1]) : null;
       const current = data[index] ? parseInt(data[index]) : null;
+      const next = data[index + 1] ? parseInt(data[index + 1]) : null;
+      const secondNext = data[index + 2] ? parseInt(data[index + 2]) : null;
 
       if (previous !== null && current !== null && previous < current) {
         largerCount++;
+      }
+
+      if (current !== null && next !== null && secondNext !== null) {
+        sumArray.push((current + next + secondNext));
+
+        const previousSum: number | undefined = sumArray.at(-2);
+        const currentSum: number | undefined = sumArray.at(-1);
+
+        if (previousSum !== undefined && currentSum !== undefined && previousSum < currentSum) {
+          sumLargerCount++;
+        }
       }
     }
 
     console.log('How many measurements are larger than the previous measurement?');
     console.log('The answer is %d', largerCount);
+
+    console.log('Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?');
+    console.log('The answer is %d', sumLargerCount);
   });
 }
 
